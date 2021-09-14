465c992960eeb7d5152bf257c5582e031c41f474
orald <orald.hysaj@hotmail.com>
2021-09-14 09:47:49 +0000

    remove old stuf

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p%5qt^s@3lc^*b(!2z!pk-7uizbjc=-%g%$d_(-2pw*wj2fxs$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['orald.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 
 MEDIA_URL = '/media/'
